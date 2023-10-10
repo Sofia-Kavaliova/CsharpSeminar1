55c7f68c84fef8bf8cdffe48cb0945818b15d822
Sofia <kovaleva.sonia.02@mail.ru>
2023-10-10 09:11:09 +0300

    Add decision Task004

---
+++ Task_004/Program.cs
@@ -0,0 +1,11 @@
+﻿﻿// Код, который на вход принимает число (number),
+// а на выходе выводит все чётные числа от 1 до number (включительно),
+// разделеные знаком табуляции.
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine ());
+
+for (int N=2; N<=number; N+=2)
+{
+    Console.Write(N + "\t");
+}
